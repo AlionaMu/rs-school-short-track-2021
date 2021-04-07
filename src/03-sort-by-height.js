ec2717becc8a5a1dc341bc2a6760345430c59559
AlionaMu <34584155+AlionaMu@users.noreply.github.com>
2021-04-07 15:00:22 +0300

    init: start task

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,25 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const one = [];
+  const sorted = [];
+  const total = [];
+  arr.forEach((el, i) => {
+    if (el === -1) {
+      one.push(i);
+    } else { sorted.push(el); }
+  });
+
+  sorted.sort((a, b) => a - b);
+
+  for (let j = 0; j < arr.length; j++) {
+    if (one.includes(j)) {
+      total.push(-1);
+    } else { total.push(sorted.shift()); }
+  }
+
+  return total;
 }
 
 module.exports = sortByHeight;
